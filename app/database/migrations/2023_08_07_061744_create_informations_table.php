a2181a3d94fc4285b826f8339284716d7f9ee77a
DESKTOP-349T7UA\tktk1 <tktk19831025@gmail.com>
2023-08-10 18:05:47 +0900

    informationsテーブルimageカラムをimage_urlに変更

--- app/database/migrations/2023_08_07_061744_create_informations_table.php
+++ app/database/migrations/2023_08_07_061744_create_informations_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('phone');
-            $table->string('image')->nullable();;
+            $table->string('image_url')->nullable();;
             $table->text('message');
             //gendersテーブルのidを外部キー設定
             $table->foreignId('gender_id')->constrained();
